48f396285ec380ea309a999a566211a6c6f30a7f
Mihails Popovs <mihails.popovs@encontrol-baltic.lv>
2020-01-16 14:16:53 +0200

    Added JsonTranslationService

---
+++ TranslationsMigrator/Models/JsonTranslationDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TranslationsMigrator.Models
+{
+	public class JsonTranslationDto
+	{
+		[JsonPropertyName("German")]
+		public string Origin { get; set; }
+		
+		public string Translation { get; set; }
+	}
+}
---
+++ TranslationsMigrator/Services/IJsonTranslationService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using TranslationsMigrator.Models;
+
+namespace TranslationsMigrator.Services
+{
+	public interface IJsonTranslationService
+	{
+		Task<IEnumerable<JsonTranslationDto>> ReadAsync(string path, CancellationToken cancellationToken = default);
+	}
+}
