498530c767e70a5f4e956b8484f7010feacef6d6
Mihails Popovs <mihails.popovs@encontrol-baltic.lv>
2020-01-16 13:55:30 +0200

    Added project

---
+++ TranslationsMigrator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TranslationsMigrator
+{
+	internal static class Program
+	{
+		private static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
