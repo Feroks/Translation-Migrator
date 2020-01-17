689a813f2fc7c5dc73fdacc7c97e4cca766b7768
Mihails Popovs <mihails.popovs@encontrol-baltic.lv>
2020-01-17 07:41:03 +0200

    Add Request and RequestHandler to create Resource File

---
+++ TranslationsMigrator/Commands/CreateResourceFileRequest.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace TranslationsMigrator.Commands
+{
+	public class CreateResourceFileRequest : IRequest
+	{
+		public CreateResourceFileRequest(Options options)
+		{
+			Options = options;
+		}
+		
+		public Options Options { get; }
+	}
+}
---
+++ TranslationsMigrator/Commands/CreateResourceFileRequestHandler.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using TranslationsMigrator.Models;
+using TranslationsMigrator.Services;
+
+namespace TranslationsMigrator.Commands
+{
+	[UsedImplicitly]
+	public class CreateResourceFileRequestHandler : AsyncRequestHandler<CreateResourceFileRequest>
+	{
+		private readonly ILogger<CreateResourceFileRequestHandler> _logger;
+		private readonly IJsonTranslationService _jsonTranslationService;
+		private readonly IResourceService _resourceService;
+
+		public CreateResourceFileRequestHandler(
+			ILogger<CreateResourceFileRequestHandler> logger,
+			IJsonTranslationService jsonTranslationService,
+			IResourceService resourceService)
+		{
+			_logger = logger;
+			_jsonTranslationService = jsonTranslationService;
+			_resourceService = resourceService;
+		}
+
+		protected override async Task Handle(CreateResourceFileRequest request, CancellationToken cancellationToken)
+		{
+			var options = request.Options;
+			var source = options.Source;
+			var origin = options.Origin;
+			var destination = options.Destination;
+
+			_logger.LogInformation("Starting");
+			
+			_logger.LogInformation("Reading Translation file at: {filePath}", source);
+			var translations = await _jsonTranslationService
+				.ReadAsync(source, cancellationToken)
+				.ConfigureAwait(false);
+
+			_logger.LogInformation("Reading Origin Resource file at: {filePath}", origin);
+			var resourceValues = await _resourceService
+				.ReadAsync(origin, cancellationToken)
+				.ConfigureAwait(false);
+
+			_logger.LogInformation("Writing Resource file at: {filePath}", destination);
+			var values = resourceValues
+				.Select(x =>
+				{
+					var value = translations
+						.SingleOrDefault(y => y.Origin == x.Value)
+						?.Translation;
+
+					return new ResourceValueDto(x.Key, value ?? string.Empty);
+				});
+
+			await _resourceService
+				.WriteAsync(destination, values, cancellationToken)
+				.ConfigureAwait(false);
+		}
+	}
+}
